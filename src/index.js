7469b90d23061c3182fca483ce5e6b6dff7c9739
MUTHUKUMAR G <139162239+muthu-2005@users.noreply.github.com>
2025-01-10 17:41:57 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom'; // Import BrowserRouter
+import {HashRouter as Router } from 'react-router-dom'; // Import BrowserRouter
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
